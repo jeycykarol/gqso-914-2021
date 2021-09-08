a22e8108adb3ad24d0b3b93e2a1650bd4fad4b7f
Jeycy Karol <jeycysilva6@gmail.com>
2021-09-08 01:56:31 +0000

    Resolvendo comentários da revisão do PR

--- calc-api/src/main/java/app/Soma.java
+++ calc-api/src/main/java/app/Soma.java
@@ -12,18 +12,28 @@ public class Soma{
 
         try {
             
-            //Converter números para double (verificar erros):
+            //Converter parâmetros (num1 e num2) para double:
             double numero1 = Double.parseDouble(num1);
             double numero2 = Double.parseDouble(num2);
-            double sum = 0;
+            double soma = 0;
 
             //Calcular a soma dos dois números:
-            sum = numero1 + numero2;
+            soma = numero1 + numero2;
 
             //Retornar soma para o usuário:
-            return String.format("%.2f", sum);
-
+            return String.format("%.2f", soma);
+        
         } catch (NumberFormatException nfe) {
+
+             /*
+            Quando o formato dos parâmetros informados na URL não estiver correto, ocorrerá uma exceção
+            que irá interromper o fluxo normal do programa.
+            Por isso o NumberFormatException é lançado para indicar que a aplicação tentou converter a 
+            string para double, mas como a string (parâmetro) informado não tinha o formato 
+            apropriado não foi possível fazer a conversão, por isso é apresentada a mensagem
+            "valores inválidos".
+            */
+
             throw new BadRequestException(String.format("Valores inválidos (%s e %s)", num1, num2));
         }
     }
